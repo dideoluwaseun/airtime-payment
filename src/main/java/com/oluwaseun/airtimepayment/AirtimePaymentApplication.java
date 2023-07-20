3b824d42920854d821cefed3f26850e4da5a8bd9
Oluwaseun Oyinloye <oluwaseun.oyinloye@globalaccelerex.com>
2023-07-20 23:38:28 +0100

    initial commit

---
+++ src/main/java/com/oluwaseun/airtimepayment/AirtimePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.oluwaseun.airtimepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirtimePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirtimePaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oluwaseun/airtimepayment/AirtimePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oluwaseun.airtimepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirtimePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
